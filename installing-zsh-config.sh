9273ad40f454b8c26347474ece7a52698dbf9675
KaloyanYosifov <kaloqn665@gmail.com>
2021-04-03 10:51:10 +0300

    Add support for installing zsh config

--- installing-zsh-config.sh
+++ installing-zsh-config.sh
@@ -1,1 +1,15 @@
 #! /usr/bin/env bash
+
+echo "Installing zsh configuration"
+
+sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="gentoo"/' $HOME/.zshrc
+
+rm -rf $HOME/.zsh
+git clone git@github.com:KaloyanYosifov/zsh-configs.git $HOME/.zsh
+
+cat $HOME/.zshrc | grep "source \$HOME/.zsh/lead.sh"
+
+if [ $? != 0 ]; then
+    echo "#Load the lead sh" >> $HOME/.zshrc
+    echo "source \$HOME/.zsh/lead.sh" >> $HOME/.zshrc
+fi
