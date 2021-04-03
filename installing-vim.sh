03ffa7c074bebb4b558960728dd288dff6f41bf0
KaloyanYosifov <kaloqn665@gmail.com>
2021-04-03 10:38:27 +0300

    Add support for installing vim

---
+++ installing-vim.sh
@@ -0,0 +1,20 @@
+echo "Installing Vim Configuration"
+rm -rf $HOME/.vim
+git clone git@github.com:KaloyanYosifov/vim-configurations.git $HOME/.vim
+
+ROOT_PATH=$(pwd)
+
+cd $HOME/.vim
+
+# install submodules
+git submodule update --init
+
+# set config for vim
+echo "source $HOME/.vim/lead.vim" > $HOME/.vimrc 
+echo "source $HOME/.vim/ideavim.vim" > $HOME/.ideavimrc 
+
+# install 
+vim -c "PluginInstall" -c "q!"
+
+cd $ROOT_PATH
+
