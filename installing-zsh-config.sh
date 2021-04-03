eb3a576431ea51a1415947d3affac089929a158a
KaloyanYosifov <kaloqn665@gmail.com>
2021-04-03 11:10:35 +0300

    Add support for installing vi plugin

--- installing-zsh-config.sh
+++ installing-zsh-config.sh
@@ -13,3 +13,12 @@ if [ $? != 0 ]; then
     echo "#Load the lead sh" >> $HOME/.zshrc
     echo "source \$HOME/.zsh/lead.sh" >> $HOME/.zshrc
 fi
+
+# Install vi mode for zsh
+mkdir -p $HOME/.oh-my-zsh/plugins/vi-mode
+curl -L https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/vi-mode/vi-mode.plugin.zsh > $HOME/.oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
+
+# find the line whener we include plugins git and add the vim plugin to the next line
+LINE_NUMBER_TO_PUT_NEW_PLUGIN=$(($(cat $HOME/.zshrc | grep -n "plugins=(git)" | grep -Eo '^[^:]+')+1))
+sed -i -e "$LINE_NUMBER_TO_PUT_NEW_PLUGIN i\\ 
+plugins=(vi-mode)" $HOME/.zshrc
