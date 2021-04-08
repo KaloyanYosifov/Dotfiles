de13f2c6b7e406ad6e88db040d29a5556c1b874e
KaloyanYosifov <kaloqn665@gmail.com>
2021-04-08 21:57:31 +0300

    Add support for installing coc extensions on setup

--- installing-vim.sh
+++ installing-vim.sh
@@ -18,5 +18,15 @@ echo "source $HOME/.vim/ideavim.vim" > $HOME/.ideavimrc
 # install 
 vim -c "PluginInstall" -c "q!"
 
+# Install coc extensions
+mkdir -p ~/.config/coc/extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+  echo '{"dependencies":{}}'> package.json
+fi
+# Change extension names to the extensions you need
+npm install coc-json coc-tsserver coc-phpls --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+
 cd $ROOT_PATH
 
