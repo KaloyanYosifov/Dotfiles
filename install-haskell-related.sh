c5f9959d7f64da000b256189bcfeb2916aa18463
KaloyanYosifov <kaloqn665@gmail.com>
2022-05-06 10:14:18 +0200

    Fix installation to be non interactive and set the cabal and ghc versions

--- install-haskell-related.sh
+++ install-haskell-related.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-exec $SHELL -l
+export BOOTSTRAP_HASKELL_NONINTERACTIVE=true
+export BOOTSTRAP_HASKELL_CABAL_VERSION=3.6.2.0
+export BOOTSTRAP_HASKELL_GHC_VERSION=8.10.7
 
-ghcup install cabal 3.6.2.0
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+
 ghcup install hls 1.6.1.0
-ghcup install ghc 8.10.7
+ghcup set hls 1.6.1.0
