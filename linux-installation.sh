b84cb77857a6ce0662eeaff48ad1c23eef78ad91
KaloyanYosifov <kaloqn665@gmail.com>
2023-06-17 11:20:32 -0400

    Change git editor to be nvim

--- linux-installation.sh
+++ linux-installation.sh
@@ -12,4 +12,7 @@ function install_brave() {
     sudo dnf install -y brave-browser brave-keyring
 }
 
-install();
+install()
+
+# configure global editor for vim
+git config --global core.editor "vim"
