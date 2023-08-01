24e7c4465963a1010ba76cd46a642af3cc612ef2
KaloyanYosifov <kaloqn665@gmail.com>
2023-08-01 23:21:50 +0200

    Support mako config

--- linux/config/install.sh
+++ linux/config/install.sh
@@ -6,5 +6,12 @@ if [[ -d $HOME/.config/sway ]]; then
     rm -rf $HOME/.config/sway 
 fi
 
+if [[ -d $HOME/.config/mako ]]; then
+    rm -rf $HOME/.config/mako
+fi
+
 ln -s $SCRIPT_DIR/sway $HOME/.config/sway
 echo "Sway config installed"
+
+ln -s $SCRIPT_DIR/mako $HOME/.config/mako
+echo "Mako config installed"
