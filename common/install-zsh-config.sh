27d800d0a28df99247c3ad47776ed07702e17fe7
KaloyanYosifov <kaloqn665@gmail.com>
2023-08-03 22:34:52 +0200

    Do not install oh my zsh

--- common/install-zsh-config.sh
+++ common/install-zsh-config.sh
@@ -2,12 +2,12 @@
 
 echo "Installing zsh configuration"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="gentoo"/' $HOME/.zshrc
-
 rm -rf $HOME/.zsh
 git clone https://github.com/KaloyanYosifov/zsh-configs.git $HOME/.zsh
+
+if ! [[ -f $HOME/.zshrc ]]; then
+    touch $HOME/.zshrc
+fi
 
 cat $HOME/.zshrc | grep "source \$HOME/.zsh/lead.sh"
 
@@ -15,18 +15,3 @@ if [ $? != 0 ]; then
     echo "#Load the lead sh" >> $HOME/.zshrc
     echo "source \$HOME/.zsh/lead.sh" >> $HOME/.zshrc
 fi
-
-cat $HOME/.zshrc | grep "plugins=(vi-mode)"
-
-if [ $? == 0 ]; then
-    exit 0
-fi
-
-# Install vi mode for zsh
-mkdir -p $HOME/.oh-my-zsh/plugins/vi-mode
-curl -L https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/vi-mode/vi-mode.plugin.zsh > $HOME/.oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
-
-# find the line whener we include plugins git and add the vim plugin to the next line
-LINE_NUMBER_TO_PUT_NEW_PLUGIN=$(($(cat $HOME/.zshrc | grep -n "plugins=(git)" | grep -Eo '^[^:]+')+1))
-sed -i -e "$LINE_NUMBER_TO_PUT_NEW_PLUGIN i\\ 
-plugins=(vi-mode)" $HOME/.zshrc
