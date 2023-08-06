c521f669ee03e6b25676096391b96c27fee6cf13
KaloyanYosifov <kaloqn665@gmail.com>
2023-08-06 12:32:55 +0200

    Add a generic function to ask if we should install or not and refactor

--- linux/install.sh
+++ linux/install.sh
@@ -5,7 +5,7 @@ set -eu
 SCRIPT_DIR=$( cd -- $( dirname -- ${BASH_SOURCE[0]} ) &> /dev/null && pwd )
 PARENT_DIR=$SCRIPT_DIR/..
 
-function add_repos() {
+function add_repos {
     repos="https://rpm.librewolf.net/librewolf-repo.repo"
     repos+=" https://download.opensuse.org/repositories/network:im:signal/Fedora_38/network:im:signal.repo"
     repos+=" https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo"
@@ -15,19 +15,28 @@ function add_repos() {
     done
 }
 
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+function ask_install {
+    echo "$1 y/n"
+    read should_install
 
-sudo dnf update -y
-sudo dnf groupinstall -y "Development Tools"
-sudo dnf install -y dnf-plugins-core neovim zsh g++ fzf apfs-fuse xclip ripgrep irssi copyq sway alacritty \
-    jetbrains-mono-fonts-all wlsunset bemenu mako htop librewolf feh qrencode ansible helm kubernetes-client \
-    keepassxc signal-desktop brave-browser brave-keyring zathura mpv git-crypt
+    return [ $should_install = "y" ] || [ $should_install = "yes" ]
+}
 
-# Install bitwarden
-sudo dnf install -y <<< curl https://github.com/bitwarden/clients/releases/download/desktop-v2023.7.1/Bitwarden-2023.7.1-x86_64.rpm
+function install_packages {
+    sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-# install mullvad
-sudo dnf install -y <<< https://mullvad.net/media/app/MullvadVPN-2023.4_x86_64.rpm
+    sudo dnf update -y
+    sudo dnf groupinstall -y "Development Tools"
+    sudo dnf install -y dnf-plugins-core neovim zsh g++ fzf apfs-fuse xclip ripgrep irssi copyq sway alacritty \
+        jetbrains-mono-fonts-all wlsunset bemenu mako htop librewolf feh qrencode ansible helm kubernetes-client \
+        keepassxc signal-desktop brave-browser brave-keyring zathura mpv git-crypt
+
+    # Install bitwarden
+    sudo dnf install -y <<< curl https://github.com/bitwarden/clients/releases/download/desktop-v2023.7.1/Bitwarden-2023.7.1-x86_64.rpm
+
+    # install mullvad
+    sudo dnf install -y <<< https://mullvad.net/media/app/MullvadVPN-2023.4_x86_64.rpm
+}
 
 function install_volta {
     echo "Installing volta"
@@ -37,12 +46,7 @@ function install_volta {
 }
 
 function install_syncthing {
-    echo "Do you want to install syncthing? y/n"
-    read should_install_syncthing
-
-    if [[ $install_additional == "y" ]] || [[ $install_additional == "yes" ]]; then
-        $SCRIPT_DIR/install-syncthing.sh
-    fi
+    $SCRIPT_DIR/install-syncthing.sh
 }
 
 function install_lf {
@@ -57,23 +61,14 @@ function install_fonts {
     $SCRIPT_DIR/install-fonts.sh
 }
 
-install_volta
-install_syncthing
-install_lf
-install_fonts
+function install_common_configuration {
+    echo "Installing linux configuration!"
+    $SCRIPT_DIR/config/install.sh
+    $PARENT_DIR/common/config/install.sh
+    $PARENT_DIR/common/link-custom-binaries.sh
+}
 
-# configure global editor for vim
-git config --global core.editor "vim"
-
-echo "Installing linux configuration!"
-$SCRIPT_DIR/config/install.sh
-$PARENT_DIR/common/config/install.sh
-$PARENT_DIR/common/link-custom-binaries.sh
-
-echo "Do you want to install additional configuration? y/n"
-read install_additional
-
-if [[ $install_additional == "y" ]] || [[ $install_additional == "yes" ]]; then
+function install_common_configuration {
     echo "Installing additional configuration"
     $PARENT_DIR/common/install-vim.sh
     $PARENT_DIR/common/install-ideavim.sh
@@ -83,4 +78,12 @@ if [[ $install_additional == "y" ]] || [[ $install_additional == "yes" ]]; then
 
     # Not so into Haskell for now
     # $PARENT_DIR/common/install-haskell.sh
-fi
+}
+
+install_packages
+install_volta
+ask_install "Do you want to install syncthing?" && install_syncthing
+install_lf
+install_fonts
+install_common_configuration
+ask_install "Do you want to install additional configuration?" && install_additional_configuration
