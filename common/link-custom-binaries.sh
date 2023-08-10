5b43ed892e8537242471420b0ec710a94ecf2b88
KaloyanYosifov <kaloqn665@gmail.com>
2023-08-10 17:36:56 +0200

    Move bin to common and update script

--- common/link-custom-binaries.sh
+++ common/link-custom-binaries.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 SCRIPT_DIR=$( cd -- $( dirname -- ${BASH_SOURCE[0]} ) &> /dev/null && pwd )
-PARENT_DIR=$SCRIPT_DIR/..
 
 mkdir -p $HOME/.local/bin || true
-ln -s $PARENT_DIR/bin/* $HOME/.local/bin || true
+ln -s $SCRIPT_DIR/bin/* $HOME/.local/bin || true
+
+echo "Installed custom binaries"
