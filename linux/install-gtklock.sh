54301183bb4ae20dd294ba99dddab68f15840578
KaloyanYosifov <kyosifovs@proton.me>
2023-12-06 22:01:04 +0100

    Update the install script

--- linux/install-gtklock.sh
+++ linux/install-gtklock.sh
@@ -7,13 +7,12 @@ PARENT_DIR=$SCRIPT_DIR/..
 
 sudo dnf install -y gcc meson pkgconf scdoc pam-devel wayland-devel gtk3-devel gtk-layer-shell-devel
 
-cd $SCRIPT_DIR
-git clone https://github.com/jovanlanik/gtklock.git
-cd gtklock
-
-meson setup builddir
-ninja -C builddir
-meson install -c builddir
-
-cd ..
-rm -rf gtklock
+cd $SCRIPT_DIR \
+    && git clone https://github.com/jovanlanik/gtklock.git \
+    && cd gtklock \
+    && meson setup builddir \
+    && ninja -C builddir \
+    && meson install -C builddir \
+    && cd .. \
+    && rm -rf gtklock \
+    || rm -rf gtklock
