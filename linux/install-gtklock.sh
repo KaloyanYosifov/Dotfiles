a61c3c970649ec31dc3882ef6bab21d93fc770ad
KaloyanYosifov <kyosifovs@proton.me>
2023-12-06 21:52:16 +0100

    Update build process for gtklock

--- linux/install-gtklock.sh
+++ linux/install-gtklock.sh
@@ -5,14 +5,15 @@ set -eu
 SCRIPT_DIR=$( cd -- $( dirname -- ${BASH_SOURCE[0]} ) &> /dev/null && pwd )
 PARENT_DIR=$SCRIPT_DIR/..
 
-sudo dnf install -y gcc make pkgconf scdoc pam-devel wayland-devel gtk3-devel gtk-layer-shell-devel
+sudo dnf install -y gcc meson pkgconf scdoc pam-devel wayland-devel gtk3-devel gtk-layer-shell-devel
 
 cd $SCRIPT_DIR
 git clone https://github.com/jovanlanik/gtklock.git
 cd gtklock
 
-make
-sudo make install
+meson setup builddir
+ninja -C builddir
+meson install -c builddir
 
 cd ..
 rm -rf gtklock
