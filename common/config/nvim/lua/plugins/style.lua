395e2c2cf320ab1332a3f27ff72a51d2a57c5285
KaloyanYosifov <kaloqn665@gmail.com>
2024-08-15 07:59:09 +0200

    Add notifications support

--- common/config/nvim/lua/plugins/style.lua
+++ common/config/nvim/lua/plugins/style.lua
@@ -22,6 +22,16 @@ return {
 			},
 		},
 	},
+	{
+		"j-hui/fidget.nvim",
+		opts = {
+			integration = {
+				["nvim-tree"] = {
+					enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
+				},
+			},
+		},
+	},
 
 	--- UI
 	{
