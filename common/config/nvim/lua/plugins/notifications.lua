8636ce8945d36b1b70d53c6f40af60478f822209
KaloyanYosifov <kaloqn665@gmail.com>
2024-08-16 08:03:24 +0200

    Move to notifications

---
+++ common/config/nvim/lua/plugins/notifications.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"j-hui/fidget.nvim",
+		opts = {
+			integration = {
+				["nvim-tree"] = {
+					enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
+				},
+			},
+		},
+	},
+}
--- common/config/nvim/lua/plugins/style.lua
+++ common/config/nvim/lua/plugins/style.lua
@@ -22,16 +22,6 @@ return {
 			},
 		},
 	},
-	{
-		"j-hui/fidget.nvim",
-		opts = {
-			integration = {
-				["nvim-tree"] = {
-					enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
-				},
-			},
-		},
-	},
 
 	--- UI
 	{
