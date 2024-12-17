a0e1de44a11fb1f5a2c272b3fc3a2a4df2a6916e
KaloyanYosifov <kaloqn665@gmail.com>
2024-12-17 18:26:04 +0100

    Support installing additional fonts and configurations for sketchybar

--- macos/install.sh
+++ macos/install.sh
@@ -45,6 +45,8 @@ yabay --start-service
 skhd --start-service
 
 # Configure sketchybar
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/
 brew services start sketchybar
 
 # Disable saving to icloud
