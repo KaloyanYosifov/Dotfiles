b0b9c01c26ef7d9017ad06dbaecab10edb744b27
KaloyanYosifov <kyosifovs@proton.me>
2025-12-03 16:57:42 +0100

    Support fzf on history and update lazy lock

---
+++ common/config/zsh/plugins/3_fzf_key_bindings.zsh
@@ -0,0 +1,18 @@
+### key-bindings.zsh ###
+#     ____      ____
+#    / __/___  / __/
+#   / /_/_  / / /_
+#  / __/ / /_/ __/
+# /_/   /___/_/ key-bindings.zsh
+#
+# - $FZF_TMUX_OPTS
+# - $FZF_CTRL_T_COMMAND
+# - $FZF_CTRL_T_OPTS
+# - $FZF_CTRL_R_OPTS
+# - $FZF_ALT_C_COMMAND
+# - $FZF_ALT_C_OPTS
+
+export FZF_CTRL_T_COMMAND=""
+export FZF_CTRL_T_COMMAND=""
+
+source <(fzf --zsh)
