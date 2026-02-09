57083f3b97892f2ffc50ae75d3682d32c5ccee3e
KaloyanYosifov <kyosifovs@proton.me>
2026-02-09 08:32:06 +0100

    Update auto session

--- common/config/nvim/lua/plugins/auto-session.lua
+++ common/config/nvim/lua/plugins/auto-session.lua
@@ -5,16 +5,19 @@ return {
 		lazy = false,
 
 		keys = {
-			{ "<leader>sr", "<cmd>SessionRestore<cr>", desc = "Restore session" },
+			{ "<leader>sr", "<cmd>AutoSession restore<cr>", desc = "Restore session" },
 		},
 
 		---enables autocomplete for opts
 		---@module "auto-session"
 		---@type AutoSession.Config
 		opts = {
-			enabled = false,
-			auto_save = false,
+			enabled = true,
+			auto_save = true,
+			auto_restore = false,
 			suppressed_dirs = { "~/", "~/Downloads", "~/Documents", "/" },
+			git_use_branch_name = true,
+			git_auto_restore_on_branch_change = true,
 		},
 	},
 }
