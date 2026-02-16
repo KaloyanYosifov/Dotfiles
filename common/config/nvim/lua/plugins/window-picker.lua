ad4850e25896abe1126a540d701b343d9aa7e2e5
KaloyanYosifov <kyosifovs@proton.me>
2026-02-16 20:56:10 +0100

    Support window picker

---
+++ common/config/nvim/lua/plugins/window-picker.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"s1n7ax/nvim-window-picker",
+		name = "window-picker",
+		event = "VeryLazy",
+		version = "2.*",
+		keys = {
+			{
+				"<leader>pw",
+				function()
+					local picked_id = require("window-picker").pick_window()
+					if picked_id then
+						vim.api.nvim_set_current_win(picked_id)
+					end
+				end,
+				{ desc = "Window picker: pick window" },
+			},
+		},
+		opts = {
+			hint = "floating-big-letter",
+			show_prompt = false,
+		},
+	},
+}
