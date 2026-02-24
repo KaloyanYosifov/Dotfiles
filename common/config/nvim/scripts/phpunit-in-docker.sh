2b7699774c95840dc3667601d3f900d6f1f872d8
KaloyanYosifov <kyosifovs@proton.me>
2026-02-24 13:56:32 +0100

    Update phpunit script to work with new neotest phpunit version

--- common/config/nvim/scripts/phpunit-in-docker.sh
+++ common/config/nvim/scripts/phpunit-in-docker.sh
@@ -6,13 +6,13 @@ containerName=$NEOTEST_PHPUNIT_CONTAINER
 # detect local path and remove from args
 localPhpUnitResultPath='/tmp/phpunit-result.xml'
 argsInput=${@}
-runFile=$(echo $argsInput| awk '{print $1}')
+runFile=$(echo "$argsInput"| awk '{print $1}')
 phpTestPath=$(dirname "$runFile")
-pushd $phpTestPath > /dev/null
+pushd "$phpTestPath" > /dev/null || exit
 projectPath="$(git rev-parse --show-toplevel)"
-popd > /dev/null
+popd > /dev/null || exit
 
-subPath=$(awk -F '/vendor/' '{print $1}' <<< $projectPath)
+subPath=$(awk -F '/vendor/' '{print $1}' <<< "$projectPath")
 
 ## detect test result output
 for i in $argsInput; do
@@ -30,9 +30,9 @@ args=("${argsInput/$subPath\//}")
 args=("${args//(*}")
 
 # Detect path
-container=$(docker ps -n=-1 --filter name=$containerName --format="{{.ID}}")
-phpunitPath=$(docker exec -it $container /bin/bash -c "[ -f vendor/bin/phpunit ] && echo vendor/bin/phpunit || echo bin/phpunit" | tr -d '\r')
-dockerPath=$(docker inspect --format {{.Config.WorkingDir}} $container)
+container=$(docker ps -n=-1 --filter name="$containerName" --format="{{.ID}}")
+phpunitPath=$(docker exec -it "$container" /bin/bash -c "[ -f vendor/bin/phpunit ] && echo vendor/bin/phpunit || echo bin/phpunit" | tr -d '\r')
+dockerPath=$(docker inspect --format {{.Config.WorkingDir}} "$container")
 
 ## debug
 # echo "Raw ARGS: "${@}
@@ -43,10 +43,10 @@ dockerPath=$(docker inspect --format {{.Config.WorkingDir}} $container)
 # echo "docker exec -i "$container" php -d memory_limit=-1 "$phpunitPath" "${args[@]}
 
 # Run the tests
-docker exec -it $container sh -c "$phpunitPath -d memory_limit=-1 ${args} --log-junit=${localPhpUnitResultPath}"
+docker exec -it "$container" sh -c "$phpunitPath -d memory_limit=-1 ${args}"
 
 # copy results
 docker cp -a "$container:$localPhpUnitResultPath" "$outputPath"|- &> /dev/null
 
 # replace docker path to locals
-sed -i '_' "s#$dockerPath#$projectPath#g" $outputPath
+sed -i '_' "s#$dockerPath#$projectPath#g" "$outputPath"
